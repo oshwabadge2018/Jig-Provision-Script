a58e2bbac6883c32d45dd3851480cc9d45536440
Alex Camilo <alex.camilo@gmail.com>
2018-09-13 21:43:42 -0400

    make result more obvious

--- update_provisioner.py
+++ update_provisioner.py
@@ -3,13 +3,23 @@ import os
 import subprocess
 import time
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 print "Checking for updates.. ",
 command = "git pull"
 process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
 process.wait()
 if process.returncode==0:
-  print "Success!"
+  print bcolors.OKGREEN+"Success!"+bcolors.ENDC
+  time.sleep(5)
 else:
-  print "Failure!"
-
-time.sleep(10)
+  print bcolors.OKGREEN+"Failure!"+bcolors.ENDC
+  time.sleep(10)
