7b6426dee7007900e4d22aa2250556564f768a9f
Alex Camilo <alex.camilo@gmail.com>
2018-09-13 21:59:49 -0400

    fix

--- autoreload.py
+++ autoreload.py
@@ -3,7 +3,7 @@ import subprocess
 import time
 
 while True:
-  command = "python ui.py"
+  command = "python gui.py"
   process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
   process.wait()
   time.sleep(2)
