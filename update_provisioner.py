8afafa2790e736070b2164c5467a86651f5f860a
Alex Camilo <alex.camilo@gmail.com>
2018-09-13 21:30:00 -0400

    ui update

--- update_provisioner.py
+++ update_provisioner.py
@@ -3,14 +3,14 @@ import os
 import subprocess
 
 print "Checking for updates.. ",
-command = "git reset --hard origin/master"
+command = "git pull"
 process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
 process.wait()
 if process.returncode==0:
   print "Success!"
 else:
   print "Failure!"
-exit(-1)
+  exit(-1)
 command = "chmod a+x *.py"
 process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
 process.wait()
