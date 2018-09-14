3f604ec54c43b2c25ea21fb2d75f40c6afda2790
Alex Camilo <alex.camilo@gmail.com>
2018-09-13 23:38:28 -0400

    need a timeout

--- provision_device.py
+++ provision_device.py
@@ -1,8 +1,9 @@
 import time
 import serial
 
-ser = serial.Serial("/dev/ttyS0",115200)
+ser = serial.Serial("/dev/ttyS0",115200,timeout=2)
 ser.write("1+1\n")
+print ser.readline()
 print ser.readline()
 time.sleep(10)
 
