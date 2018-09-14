ef434e3c2709be3cc0995a5d4d7850f9ef9bd618
Alex Camilo <alex.camilo@gmail.com>
2018-09-13 21:41:16 -0400

    fix

--- update_provisioner.py
+++ update_provisioner.py
@@ -7,10 +7,9 @@ print "Checking for updates.. ",
 command = "git pull"
 process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
 process.wait()
-time.sleep(10)
 if process.returncode==0:
   print "Success!"
 else:
   print "Failure!"
-  exit(-1)
 
+time.sleep(10)
