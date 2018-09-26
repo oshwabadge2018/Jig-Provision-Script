d7d45a9495f19c05f9eb92e270465d738b8a0a22
Alex Camilo <alex.camilo@gmail.com>
2018-09-25 21:21:40 -0400

    push name to device and reset

--- provision_device.py
+++ provision_device.py
@@ -2,8 +2,11 @@ import time
 import serial
 
 ser = serial.Serial("/dev/ttyS0",115200,timeout=2)
-ser.write("\n\n\n")
-print ser.readline()
-print ser.readline()
-time.sleep(10)
 
+first = "Joe"
+last = "Schmoe"
+filew = "f = open(\"d_name.py\",\"w\")\r\nf.write(\"first='\")\r\nf.write(\"%s\")\r\nf.write(\"'\\n\")\r\nf.write(\"last='\")\r\nf.write(\"%s\")\r\nf.write(\"'\\n\")\r\nf.close()\r\n\r\n" % (first,last)
+ser.write(filew)
+ser.flush()
+ser.write("import machine\r\n")
+ser.write("machine.reset()\r\n")
